6a6073f64c7a290f75a118ba2dd9b3d4c85acb4c
Adarsh Kumar <adars99@gmail.com>
2019-01-24 07:32:00 -0800

    Update prime.js

--- Prime/prime.js
+++ Prime/prime.js
@@ -1,9 +1,11 @@
-function(n){
+function isPrime(n){
  var divisor = 2;
  
  while(n > divisor){
-  if(n / divisor) { return false; }
+  if(n % divisor === 0 ) { return false; }
   else { divisor++}
  }
  return true;
 }
+alert(isPrime(137));
+alert(isPrime(121));
